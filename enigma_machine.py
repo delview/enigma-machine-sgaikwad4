672b07b6cb23a6e29848820e3d45852e6f69fc41
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-03-05 13:05:24 -0800

    Added pseudocode
    Might change it up later

---
+++ enigma_machine.py
@@ -0,0 +1,9 @@
+# Ask user if they want to encrypt or decrypt a message
+
+# Accept a string that will either encrypt or decrypt
+
+# Encrypting function
+
+# Decrypting function
+
+# Print the final encrypted or decrypted string to the user
