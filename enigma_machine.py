b98e555d615cc24e53c53299fc688570d4f9f37e
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-03-06 09:22:07 -0800

    Explained the program and made functions
    I also made a function that guides the user to the encrypt or decrypt function based on their choice.

--- enigma_machine.py
+++ enigma_machine.py
@@ -1,9 +1,31 @@
-# Ask user if they want to encrypt or decrypt a message
 
+
+# Ask user if they want to encrypt or decrypt a message
+def choice():
+    while True:
+        choice = input("Do you want to [e]ncrypt or [d]ecrypt a message").strip().title()
+        if choice == "E":
+            encrypt()
+            break
+        elif choice== "D":
+            decrypt()
+            break
+        else:
+            print("Please enter a valid answer")
+    
 # Accept a string that will either encrypt or decrypt
 
 # Encrypting function
-
+def encrypt():
+    print("hello")
 # Decrypting function
+def decrypt():
+    print("jovn")
+
+# Tell user what cipher the program is using
+print("Hello, and welcome to the Enigma machine. This machine only works with the atbash cipher")
+choice()
 
 # Print the final encrypted or decrypted string to the user
+
+# Ask user if they want to use the program again
