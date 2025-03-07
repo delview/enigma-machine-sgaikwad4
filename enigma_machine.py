5c284394f2648cb6672273f3eec3e469295281d6
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-03-07 11:18:32 -0800

    Made dictionary to store the cipher
    Added pseudocode as well.

--- enigma_machine.py
+++ enigma_machine.py
@@ -2,6 +2,8 @@
 message = []
 final_message = []
 
+# Create a dictionary
+cipher = {"a":"z", "b":"y", "c":"x", "d":"w", "e":"v", "f":"u", "g":"t", "h":"s", "i":"r", "j":"q", "k":"p", "l":"o", "m":"n", "n":"m", "o":"l", "p":"k", "q":"j", "r":"i", "s":"h", "t":"g", "u":"f", "v":"e", "w":"d", "x":"c", "y":"b", "z":"a",}
 # Ask user if they want to encrypt or decrypt a message
 def choice():
     while True:
