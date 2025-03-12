2770ab030c4da42bd1905466645a705eb773af74
Siddhant Gaikwad <siddhantg612@deltalearns.ca>
2025-03-11 21:41:38 -0700

    Fixed an error for an error, fixed an error, and removed unnecessary pseudocode.
    Originally, when you type a message that has characters that the cipher didn't have, the program would print out an error message. But since the error message was in a loop, it would be printed as many times as there are characters in the message. Instead of asking the user for a message in the atbash function, I put it in the choice function along with the else statement.
    
    I noticed another problem. When I want to restart the program, the final_message list kept it's value instead of reseting to an empty list. I moved it inside the atbash function to fix it.
    
    Removed unnecessary pseudocode

--- enigma_machine.py
+++ enigma_machine.py
@@ -1,18 +1,17 @@
-# Make lists
-final_message = []
-
 # Create a dictionary
 cipher_atbash = {"a":"z", "b":"y", "c":"x", "d":"w", "e":"v", "f":"u", "g":"t", "h":"s", "i":"r", "j":"q", "k":"p", "l":"o", "m":"n", "n":"m", "o":"l", "p":"k", "q":"j", "r":"i", "s":"h", "t":"g", "u":"f", "v":"e", "w":"d", "x":"c", "y":"b", "z":"a",}
-
-# Create a dictionary for 
 
 # Ask user what cipher they want to use
 def choice():
     while True:
         choice = input("What cipher do you want to use? [A]tbash or [C]aesar?").strip().title()
-        if choice == "A":
-            atbash()
+        message = input("Enter a message you want to encrypt: ").strip().lower()
+        if choice == "A" and message.isalpha():
+            atbash(message)
             break
+        elif choice == "A" and not message.isalpha():
+            print("Something went wrong")
+            continue
         elif choice== "C":
             # make a loop for asking user if they want to encrypt or decrypting
             caesar_choice = input("Do you want to [E]ncrypt or [D]ecrypt").strip().title()
@@ -26,15 +25,15 @@ def choice():
         else:
             print("Please enter a valid answer")
 
-# function for atbash
-def atbash():
-    # Accept a string that will either encrypt or decrypt
-    message = input("Enter a message you want to encrypt: ").strip().lower()
+# function for atbash 
+def atbash(message):
+    
+    # Make lists
+    final_message = []
+    # Accept a string that will either encrypt or decrypt 
     for letter in message:
         if letter in cipher_atbash:
             final_message.append(cipher_atbash[letter])
-        else:
-            print("Oh no")
     print("".join(map(str, final_message)))
     restart()
 
