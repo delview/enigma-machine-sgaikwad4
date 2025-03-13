95426f7496a1015a077912c1d0c6a4454c528783
Siddhant Gaikwad <siddhantg612@deltalearns.ca>
2025-03-12 18:24:05 -0700

    Made program user friendly
    Made the program more user friendly by adding newlines where the user can enter an input. Helps make it easier for the user to read.

--- enigma_machine.py
+++ enigma_machine.py
@@ -1,11 +1,14 @@
 # Create a dictionary
 cipher_atbash = {"a":"z", "b":"y", "c":"x", "d":"w", "e":"v", "f":"u", "g":"t", "h":"s", "i":"r", "j":"q", "k":"p", "l":"o", "m":"n", "n":"m", "o":"l", "p":"k", "q":"j", "r":"i", "s":"h", "t":"g", "u":"f", "v":"e", "w":"d", "x":"c", "y":"b", "z":"a",}
 
+# Greet user
+
+
 # Ask user what cipher they want to use
 def choice():
     while True:
-        choice = input("What cipher do you want to use? [A]tbash or [C]aesar?").strip().title()
-        message = input("Enter a message you want to encrypt (please enter only letters, no special characters or numbers): ").strip().lower()
+        choice = input("What cipher do you want to use? [A]tbash or [C]aesar?\n").strip().title()
+        message = input("Enter a message you want to encrypt (please enter only letters, no special characters or numbers):\n").strip().lower()
         if choice == "A" and message.isalpha():
             atbash(message)
             break
@@ -15,7 +18,7 @@ def choice():
         elif choice== "C":
             while True:
                 # make a loop for asking user if they want to encrypt or decrypting
-                caesar_choice = input("Do you want to [E]ncrypt or [D]ecrypt").strip().title()
+                caesar_choice = input("Do you want to [E]ncrypt or [D]ecrypt\n").strip().title()
                 if caesar_choice == "E":
                     caesar_encrypt(message)
                     break
@@ -37,6 +40,7 @@ def atbash(message):
     for letter in message:
         if letter in cipher_atbash:
             final_message.append(cipher_atbash[letter])
+    print("The final message is: \n")
     print("".join(map(str, final_message)))
     restart()
 
@@ -69,7 +73,7 @@ def caesar_decrypt(message):
 # Ask user if they want to use the program again
 def restart():
     while True:
-        restart = input("Do you want to do another encryption or decryption? [Y]es or [N]o").strip().title()
+        restart = input("Do you want to do another encryption or decryption? [Y]es or [N]o\n").strip().title()
         if restart == "Y":
             choice()
             break
